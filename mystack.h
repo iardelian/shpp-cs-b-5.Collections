8444536c6efa9ffb1fe71549d107f75c53291c45
iardelyan <ardelyan.i@gmail.com>
2015-12-25 17:09:18 +0200

    fix formatting

--- mystack.h
+++ mystack.h
@@ -11,66 +11,62 @@ private:
     struct Node{
         type m_data;
         Node *previousElement; // pointer to the previous element of the stack
-
+        
         Node(){
-        //default constructor
+            //default constructor
         }
         Node(type data){ // constructor
             m_data = data;
         }
     };
-
+    
     Node *topElement = new Node();
     int stackSize;// number of elements in the stack
 public:
-    MyStack():stackSize(0){
-        // initial size of the stack is equal to zero
-    }
-    ~MyStack(){
-        // destructor
-    }
-
-    void push(type m_data){
-        Node *newElement = new Node(m_data);//create a pointer to the new element
-        if(stackSize == 0){//If there are no elements in the stack, the top of the stack is a new element
+    MyStack():stackSize(0){}// initial size of the stack is equal to zero
+    
+    ~MyStack() {}// destructor
+    
+    void push(type m_data) {
+        Node *newElement = new Node(m_data); // create a pointer to the new element
+        if (stackSize == 0) { // if there are no elements in the stack, the top of the stack is a new element
             topElement = newElement;
-        }else{
+        } else {
             newElement->previousElement = topElement; // previous element becomes the top element
             topElement = newElement;
         }
         stackSize++; // increase the number of elements after each addition of element
     }
-
+    
     type top(){ // top of the stack
         return topElement->m_data;
     }
-
+    
     void pop(){
-        if(isEmpty()){
+        if (isEmpty()) {
             Node* buff = topElement;
             topElement = topElement->previousElement;
             delete buff;
             stackSize--; // reduce the number of elements after each removal of element
-        }else{
+        } else {
             cout << "Error: stack underflow!" << endl; // if stack if isEmpty
         }
     }
-
+    
     void clear(){
-        while (stackSize!=0){
+        while (stackSize!=0) {
             Node* buff = topElement;
             topElement = topElement->previousElement;
             delete buff;
             stackSize--;
         }
     }
-
+    
     int size(){ // get stack size
         return stackSize;
     }
-
-    //if stack is empty - return true
-    bool isEmpty(){
+    
+    bool isEmpty(){ // if stack is empty - return true
         return (stackSize != 0);
     }
 };
