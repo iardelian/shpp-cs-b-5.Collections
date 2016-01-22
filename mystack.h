78c0f4cdf1a426b005134b4b88405a9a8d098689
iardelyan <ardelyan.i@gmail.com>
2016-01-22 13:38:38 +0200

    Final

--- mystack.h
+++ mystack.h
@@ -6,69 +6,104 @@
 using namespace std;
 
 template <typename type>
-class MyStack{
-private:
-    struct Node{
-        type m_data;
-        Node *previousElement; // pointer to the previous element of the stack
-        
-        Node(){
-            //default constructor
-        }
-        Node(type data){ // constructor
-            m_data = data;
-        }
-    };
-    
-    Node *topElement = new Node();
-    int stackSize;// number of elements in the stack
+class MyStack {
+
 public:
-    MyStack():stackSize(0){}// initial size of the stack is equal to zero
-    
-    ~MyStack() {}// destructor
-    
-    void push(type m_data) {
-        Node *newElement = new Node(m_data); // create a pointer to the new element
-        if (stackSize == 0) { // if there are no elements in the stack, the top of the stack is a new element
-            topElement = newElement;
-        } else {
-            newElement->previousElement = topElement; // previous element becomes the top element
-            topElement = newElement;
-        }
-        stackSize++; // increase the number of elements after each addition of element
-    }
-    
-    type top(){ // top of the stack
-        return topElement->m_data;
-    }
-    
-    void pop(){
-        if (isEmpty()) {
-            Node* buff = topElement;
-            topElement = topElement->previousElement;
-            delete buff;
-            stackSize--; // reduce the number of elements after each removal of element
-        } else {
-            cout << "Error: stack underflow!" << endl; // if stack if isEmpty
-        }
-    }
-    
-    void clear(){
-        while (stackSize!=0) {
-            Node* buff = topElement;
-            topElement = topElement->previousElement;
-            delete buff;
-            stackSize--;
-        }
-    }
-    
-    int size(){ // get stack size
-        return stackSize;
-    }
-    
-    bool isEmpty(){ // if stack is empty - return true
-        return (stackSize != 0);
-    }
+    //initializes a new empty stack
+    MyStack();
+    //destructor
+    virtual ~MyStack();
+    //put element to the stack
+    void push(type data);
+    //remove top element from the stack and show it's data
+    type pop();
+    //removes all elements from the stack
+    void clear();
+    //return true if stack is empty
+    bool isEmpty() const;
+    //show data of the top element of the stack
+    type top() const;
+    //show current number of the element in the stack
+    int size() const;
+
+private:
+    static const int START_STACK_SIZE = 10;
+    //array for storing elements
+    type *array;
+    //current size of array
+    int currentSize;
+    //if there will be not enough space for elements, size of the array will be increased by "START_STACK_SIZE" value
+    void doubleArraySize();
+    //current number of the element in array
+    int numberOfElements;
 };
+
+/* Methods implementation */
+
+template <typename type>
+MyStack<type>::MyStack(){
+    array = new type[START_STACK_SIZE];
+    currentSize = START_STACK_SIZE;
+    numberOfElements = 0; //start number of elements
+}
+
+template <typename type>
+MyStack<type>::~MyStack(){
+    delete[] array;
+}
+
+template <typename type>
+void MyStack<type>::push(type data){
+    if (numberOfElements == currentSize) { //check space for new element
+        doubleArraySize();
+    }
+    array[numberOfElements] = data;
+    numberOfElements++;
+}
+
+template <typename type>
+type MyStack<type>::pop(){
+    if (isEmpty()) {
+        cout << "Error: Stack is empty!" << endl;
+    }
+    numberOfElements--;
+    return array[numberOfElements];
+
+}
+
+template <typename type>
+void MyStack<type>::clear(){
+    numberOfElements = 0;
+}
+
+template <typename type>
+bool MyStack<type>::isEmpty() const {
+    return numberOfElements == 0;
+}
+
+template <typename type>
+type MyStack<type>::top() const {
+    if (isEmpty()) {
+        cout << "Error: Stack is empty!" << endl;
+    }
+    return array[numberOfElements-1];
+}
+
+template <typename type>
+int MyStack<type>::size() const{
+    return numberOfElements;
+}
+
+template <typename type>
+void MyStack<type>::doubleArraySize(){
+    type *tempArray = array;
+    currentSize += START_STACK_SIZE;
+    array = new type[currentSize];
+
+    for (int i = 0; i < numberOfElements; i++) {
+        array[i] = tempArray[i];
+    }
+    delete[] tempArray;
+}
 
 #endif // MYSTACK
